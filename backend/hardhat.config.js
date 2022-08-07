f8aab5a38a5edc616dbb4042b77bce823dbf43e8
Umar Esso <umarkhatab467@gmail.com>
2022-08-07 11:04:09 +0500

    Added deployment config in backend

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
 };
+
